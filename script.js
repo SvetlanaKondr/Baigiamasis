2734f5e7448587f0fd9f29f0f47e82772ecefbd3
SvetlanaKondr <kanffeettka9@gmail.com>
2025-03-17 19:49:58 +0200

    updates

--- script.js
+++ script.js
@@ -1,54 +1,74 @@
-    // const grizti = document.getElementById("grizti");
+// const grizti = document.getElementById("grizti");
 
 //perkeltas i kita puslapi
-    // if (grizti) {
-    //     grizti.addEventListener('click', function () {
-    //         console.log("paspaustas")
-    //         window.location.href = "index.html";
-    //     });
-    // }
-   
+// if (grizti) {
+//     grizti.addEventListener('click', function () {
+//         console.log("paspaustas")
+//         window.location.href = "index.html";
+//     });
+// }
 
-  // 2. Randame HTML konteinerį, kur bus prekių sąrašas
-  const productsContainer = document.querySelector(".products");
-  const prekiuSelect = document.getElementById("prekiu-sarasas");
 
-    //prekiu kintamosios
-    // 1. Sukuriame masyvą su prekių informacija
-    if (!productsContainer) {
-        console.log("Šiame puslapyje nėra .products, todėl produktai nebus sugeneruoti.");
-    } else {
+// 2. Randame HTML konteinerį, kur bus prekių sąrašas
+const productsContainer = document.querySelector(".products");
+const prekiuSelect = document.getElementById("prekiu-sarasas");
+
+//prekiu kintamosios
+// 1. Sukuriame masyvą su prekių informacija
+if (!productsContainer) {
+    console.log("Šiame puslapyje nėra .products, todėl produktai nebus sugeneruoti.");
+} else {
     const prekes = [
-        { id: "X001", kaina: 10, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek1.jpg" },
-        { id: "X002", kaina: 11, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek2.jpg" },
-        { id: "X003", kaina: 12, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek3.jpg" },
-        { id: "X004", kaina: 13, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek4.jpg" }
+        { id: "X001", kaina: 10, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek1.jpg", rezervuota: false },
+        { id: "X002", kaina: 11, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek2.jpg", rezervuota: true },
+        { id: "X003", kaina: 12, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek3.jpg", rezervuota: false },
+        { id: "X004", kaina: 13, aprasymas: "Trumpas prekės aprašymas.", paveikslelis: "images/prek4.jpg", rezervuota: false }
     ];
 
-  //issaugome prelkes local storage, kad galetume perduoti prekiu sarasa kitam puslapiui
+    //issaugome prelkes local storage, kad galetume perduoti prekiu sarasa kitam puslapiui
 
     localStorage.setItem("prekes", JSON.stringify(prekes));
 
     // 3.  Dinamiškai sukuriame prekių HTML kodą
     prekes.forEach(preke => {
 
-//prekiu masyvas
+        //prekiu masyvas
         const productDiv = document.createElement("div");
         productDiv.classList.add("product");
 
+
         productDiv.innerHTML = `
-        <img src="${preke.paveikslelis}" alt="${preke.id}">
+        <div id="rezervuotas-${preke.id}"><img src="${preke.paveikslelis}" alt="${preke.id}"></div>
         <h3>${preke.id}</h3>
         <h4>Kaina: ${preke.kaina} Eur</h4>
         <p>${preke.aprasymas}</p>
     `;
 
+
+        //     productDiv.innerHTML = `
+        //     <img src="${preke.paveikslelis}" alt="${preke.id}">
+        //     <h3>${preke.id}</h3>
+        //     <h4>Kaina: ${preke.kaina} Eur</h4>
+        //     <p>${preke.aprasymas}</p>
+        // `;
+
         // 4.  Pridedame sukurtą prekę į `.products` konteinerį
         productsContainer.appendChild(productDiv);
+
+        //sukurtas rezervavimo elementas
+        if (preke.rezervuota) {
+            productDiv.classList.add("rezervuota")
+            const divas = document.createElement("div")
+            divas.innerText = 'Rezervuota'
+            const rezervuotasDivas = document.getElementById(`rezervuotas-${preke.id}`)
+            rezervuotasDivas.appendChild(divas)
+        }
+
+
     });
 
     //Paspaudimas, kad prekes padidetu
-   
+
     document.addEventListener("DOMContentLoaded", function () {
         const products = document.querySelectorAll('.product');
 
@@ -59,6 +79,6 @@
         });
 
     });
-    }
-  
+}
+
 
